0335cea7ff940b1ba1e8203e8b4f6ce7bcea1b48
Davide Botrugno <davide.botrugno@linksmt.it>
2023-11-09 14:57:55 +0100

    Init.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Catalogo from "./components/catalogo/catalogo";
+
+function App() {
+    return (
+        <Catalogo />
+    );
+}
+
+export default App;
---
+++ src/components/catalogo/catalogo.tsx
@@ -0,0 +1,15 @@
+interface IProps {
+
+}
+
+const Catalogo: React.FC<IProps> = (props: IProps): React.ReactElement => {
+
+
+    return (
+        <>
+            Test
+        </>
+    );
+};
+
+export default Catalogo;
